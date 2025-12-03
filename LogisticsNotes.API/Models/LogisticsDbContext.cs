2845ca091c09599a1bbbffa3371fee647158abe8
Abdulrahman Hamdi <93606366+abdulrahmanhamdi@users.noreply.github.com>
2025-12-03 12:52:26 +0300

    modified codes

--- LogisticsNotes.API/Models/LogisticsDbContext.cs
+++ LogisticsNotes.API/Models/LogisticsDbContext.cs
@@ -16,51 +16,29 @@ public partial class LogisticsDbContext : DbContext
     }
 
     public virtual DbSet<Attachment> Attachments { get; set; }
-
     public virtual DbSet<Branch> Branches { get; set; }
-
     public virtual DbSet<Category> Categories { get; set; }
-
     public virtual DbSet<Courier> Couriers { get; set; }
-
     public virtual DbSet<DeliveryHistory> DeliveryHistories { get; set; }
-
     public virtual DbSet<Folder> Folders { get; set; }
-
     public virtual DbSet<Note> Notes { get; set; }
-
     public virtual DbSet<Payment> Payments { get; set; }
-
     public virtual DbSet<PaymentMethod> PaymentMethods { get; set; }
-
     public virtual DbSet<Role> Roles { get; set; }
-
     public virtual DbSet<ServiceType> ServiceTypes { get; set; }
-
     public virtual DbSet<SharedNote> SharedNotes { get; set; }
-
     public virtual DbSet<Shipment> Shipments { get; set; }
-
     public virtual DbSet<ShipmentStatus> ShipmentStatuses { get; set; }
-
     public virtual DbSet<Tag> Tags { get; set; }
-
     public virtual DbSet<User> Users { get; set; }
-
     public virtual DbSet<Vehicle> Vehicles { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=LogisticsNotesDB;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Attachment>(entity =>
         {
             entity.HasKey(e => e.AttachmentId).HasName("PK__Attachme__442C64DE2195BA66");
-
             entity.Property(e => e.UploadedAt).HasDefaultValueSql("(getdate())");
-
             entity.HasOne(d => d.Note).WithMany(p => p.Attachments).HasConstraintName("FK_Attachments_Notes");
         });
 
@@ -77,11 +55,8 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<Courier>(entity =>
         {
             entity.HasKey(e => e.CourierId).HasName("PK__Couriers__CDAE76F655620BAA");
-
             entity.Property(e => e.IsActive).HasDefaultValue(true);
-
             entity.HasOne(d => d.CurrentBranch).WithMany(p => p.Couriers).HasConstraintName("FK_Couriers_Branches");
-
             entity.HasOne(d => d.User).WithOne(p => p.Courier)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Couriers_Users");
@@ -90,11 +65,8 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<DeliveryHistory>(entity =>
         {
             entity.HasKey(e => e.HistoryId).HasName("PK__Delivery__4D7B4ADD4A2B98BD");
-
             entity.Property(e => e.Timestamp).HasDefaultValueSql("(getdate())");
-
             entity.HasOne(d => d.Shipment).WithMany(p => p.DeliveryHistories).HasConstraintName("FK_History_Shipments");
-
             entity.HasOne(d => d.Status).WithMany(p => p.DeliveryHistories)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_History_Statuses");
@@ -103,9 +75,7 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<Folder>(entity =>
         {
             entity.HasKey(e => e.FolderId).HasName("PK__Folders__ACD7109FC34BF54F");
-
             entity.Property(e => e.IsArchived).HasDefaultValue(false);
-
             entity.HasOne(d => d.User).WithMany(p => p.Folders)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Folders_Users");
@@ -114,13 +84,9 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<Note>(entity =>
         {
             entity.HasKey(e => e.NoteId).HasName("PK__Notes__EACE357F30076034");
-
             entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
-
             entity.HasOne(d => d.Category).WithMany(p => p.Notes).HasConstraintName("FK_Notes_Categories");
-
             entity.HasOne(d => d.Folder).WithMany(p => p.Notes).HasConstraintName("FK_Notes_Folders");
-
             entity.HasOne(d => d.User).WithMany(p => p.Notes)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Notes_Users");
@@ -146,14 +112,11 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<Payment>(entity =>
         {
             entity.HasKey(e => e.PaymentId).HasName("PK__Payments__9B556A58F084C8F7");
-
             entity.Property(e => e.IsSuccessful).HasDefaultValue(true);
             entity.Property(e => e.PaymentDate).HasDefaultValueSql("(getdate())");
-
             entity.HasOne(d => d.Method).WithMany(p => p.Payments)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Payments_Methods");
-
             entity.HasOne(d => d.Shipment).WithMany(p => p.Payments)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Payments_Shipments");
@@ -177,7 +140,6 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<SharedNote>(entity =>
         {
             entity.HasKey(e => e.ShareId).HasName("PK__SharedNo__D32A3F8ECEF9DCF5");
-
             entity.Property(e => e.PermissionLevel).HasDefaultValue("View");
 
             entity.HasOne(d => d.Note).WithMany(p => p.SharedNotes)
@@ -192,7 +154,6 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<Shipment>(entity =>
         {
             entity.HasKey(e => e.ShipmentId).HasName("PK__Shipment__5CAD378DE24EF432");
-
             entity.Property(e => e.SendingDate).HasDefaultValueSql("(getdate())");
 
             entity.HasOne(d => d.CurrentStatus).WithMany(p => p.Shipments)
@@ -229,9 +190,7 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.UserId).HasName("PK__Users__1788CCAC35399D23");
-
             entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
-
             entity.HasOne(d => d.Role).WithMany(p => p.Users)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Users_Roles");
@@ -240,7 +199,6 @@ public partial class LogisticsDbContext : DbContext
         modelBuilder.Entity<Vehicle>(entity =>
         {
             entity.HasKey(e => e.VehicleId).HasName("PK__Vehicles__476B54B2844F34A2");
-
             entity.Property(e => e.Status).HasDefaultValue("Available");
         });
 
--- LogisticsNotes.API/Program.cs
+++ LogisticsNotes.API/Program.cs
@@ -1,21 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using LogisticsNotes.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<LogisticsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
