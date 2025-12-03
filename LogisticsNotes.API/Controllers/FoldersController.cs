642a2808b35000b801066ed17ae48fe75daa4440
Abdulrahman Hamdi <93606366+abdulrahmanhamdi@users.noreply.github.com>
2025-12-03 15:55:46 +0300

    added new controller

---
+++ LogisticsNotes.API/Controllers/FoldersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LogisticsNotes.API.Models;
+
+namespace LogisticsNotes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoldersController : ControllerBase
+    {
+        private readonly LogisticsDbContext _context;
+
+        public FoldersController(LogisticsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Folders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Folder>>> GetFolders()
+        {
+            return await _context.Folders.ToListAsync();
+        }
+
+        // GET: api/Folders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Folder>> GetFolder(int id)
+        {
+            var folder = await _context.Folders.FindAsync(id);
+
+            if (folder == null)
+            {
+                return NotFound();
+            }
+
+            return folder;
+        }
+
+        // PUT: api/Folders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFolder(int id, Folder folder)
+        {
+            if (id != folder.FolderId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(folder).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FolderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Folders
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Folder>> PostFolder(Folder folder)
+        {
+            _context.Folders.Add(folder);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFolder", new { id = folder.FolderId }, folder);
+        }
+
+        // DELETE: api/Folders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFolder(int id)
+        {
+            var folder = await _context.Folders.FindAsync(id);
+            if (folder == null)
+            {
+                return NotFound();
+            }
+
+            _context.Folders.Remove(folder);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FolderExists(int id)
+        {
+            return _context.Folders.Any(e => e.FolderId == id);
+        }
+    }
+}
